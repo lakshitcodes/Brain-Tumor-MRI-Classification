64a04abeed3ef6117720e7e81616e9cc21da4eb5
Lakshit Jain <jainlakshit849@gmail.com>
2025-08-24 18:22:29 +0530

    Prepare Base Model Stage Completed

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ from brainTumorMRIClassification import logger
 from brainTumorMRIClassification.pipeline.stage_01_data_ingestion import (
     DataIngestionTrainingPipeline,
 )
+from brainTumorMRIClassification.pipeline.stage_02_prepare_base_model import (
+    PrepareBaseModelTrainingPipeline,
+)
 
 STAGE_NAME = "Data Ingestion Pipeline"
 try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/brainTumorMRIClassification/config/configuration.py
+++ src/brainTumorMRIClassification/config/configuration.py
@@ -3,7 +3,10 @@ from brainTumorMRIClassification.utils.common import read_yaml, create_directori
 from pathlib import Path
 from brainTumorMRIClassification import logger
 from brainTumorMRIClassification.constants import *
-from brainTumorMRIClassification.entity.config_entity import DataIngestionConfig
+from brainTumorMRIClassification.entity.config_entity import (
+    DataIngestionConfig,
+    PrepareBaseModelConfig,
+)
 
 
 class ConfigurationManager:
@@ -28,3 +31,20 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_seed=self.params.SEED,
+            params_classes=self.params.CLASSES,
+        )
+
+        return prepare_base_model_config
